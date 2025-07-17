713c04a759197be697e3a16018a95441b70c330e
sergio <ssergiosantos088@gmail.com>
2025-07-17 13:36:11 -0300

    About

--- src/componentes/T1.tsx
+++ src/componentes/T1.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function T1({ children }: { children: React.ReactNode }) {
   return (
-    <div>
+    <div className="lg:hidden ">
       <h2 className="text-slate-200 text-[20px]">{children}</h2>
     </div>
   );
--- src/componentes/T2.tsx
+++ src/componentes/T2.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function T2({ children }: { children: React.ReactNode }) {
   return (
     <div>
-      <p className="text-slate-400 max-w-xs ">{children}</p>
+      <p className="text-slate-400 max-w-xs lg:max-w-full">{children}</p>
     </div>
   );
 }
